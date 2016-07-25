11feb6b34cb00ec24e6127ae89b86c327c1b3820
chirag <dummy5trial@gmail.com>
2016-07-25 01:36:40 +0000

    Completed auth model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,13 +13,12 @@ class SessionsController < ApplicationController
       flash.now[:danger] = "There was something wrong with your login information"
       render 'new'
     end
-  end
+   end
 
- def destroy
-   session[:user_id] = nil
-   flash[:success] = "You have logged out"
-   redirect_to root_path
- end
-
+   def destroy
+     session[:user_id] = nil
+     flash[:success] = "You have logged out"
+     redirect_to root_path
+   end
 end
 
